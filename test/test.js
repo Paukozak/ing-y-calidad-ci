e6f04d9ddb517d8b0020468709a3d64a074252c3
Paula <paukozakr@gmail.com>
2026-05-21 21:40:28 -0300

    primer commit :)

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+// test: el archivo index.html existe
+if (!fs.existsSync("./index.html")) {
+  console.error("FALLO: index.html no existe");
+  process.exit(1);
+}
+
+// test: el archivo contiene la palabra "Hola"
+const contenido = fs.readFileSync("./index.html", "utf8");
+console.log("ESTO ES LO QUE LEE EL SCRIPT:\n", contenido);
+if (!contenido.includes("Hola")) {
+  console.error("FALLO: index.html no contiene la palabra 'Hola'");
+  process.exit(1);
+}
+
+
+console.log("OK: index.html existe y contiene 'Hola'");
